5a13915608eca17e9255311f8676a1b60c44d7e9
Sav1taRG <sav1tar@me.com>
2022-10-02 22:10:07 +0200

    Logic for Login button added. Logic for Logout button added. Failed login alert added.

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,13 +9,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet var loginTF: UITextField!
+    @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
     @IBOutlet var loginButton: UIButton!
     
+    private let userName = "Test"
+    private let password = "Test"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    @IBAction func logintButtonPressed() {
+        if userNameTF.text != userName || passwordTF.text != password {
+        showLoginAlert(title: "Login Failed.", message: "Please, enter correct login and password.", textField: passwordTF)
+        }
+    }
+    
+    private func showLoginAlert (title: String, message: String, textField: UITextField) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+            textField.text = ""
+        })
+        present(alert, animated: true)
     }
 }
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
 }
+
