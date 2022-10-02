b5d2098d01a2d578d36c4cc75534671512a634bf
Sav1taRG <sav1tar@me.com>
2022-10-02 19:38:32 +0200

    UI constraints added

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Roman Golubinko on 02.10.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var loginTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  LoginApp
 //
 //  Created by Roman Golubinko on 02.10.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WelcomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
